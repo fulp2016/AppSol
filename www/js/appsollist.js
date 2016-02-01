b4a7adb0bf4fd9416e78f60c89e68b535126e747
fulp2016 <webmaster@fulp.es>
2016-02-01 11:45:47 +0000

    v1

---
+++ www/js/appsollist.js
@@ -0,0 +1,107 @@
+var serviceDestacadosURL = "http://www.fulp.es/servicesfulp/ofertas.json";
+
+var employees;
+
+
+$('#appsolListPage').bind('pageinit', function(event) {
+	getDestacadosList();
+});
+
+
+
+
+function getDestacadosList() {
+	mostrarDestacados();
+	$.getJSON(serviceDestacadosURL, function(data) {
+		$('#destacadosList li').remove();
+		employees = data;
+		$.each(employees, function(index, destacado) {
+			$('#destacadosList').append('<li><a href="destacadodetails.html?id=' + destacado.id_solicitud + '">' +
+					'<h4>' + destacado.asunto + '</h4>' +
+					'<p>' + destacado.entidad + '</p>' +
+					'<span class="precioantes">' + destacado.contrato + ' &euro;</span><span class="preciooferta">' + destacado.salario + ' &euro;</span></a></li>');
+		});
+		$('#destacadosList').listview('refresh');
+	});
+}
+
+
+function getCitaForm() {
+	mostrarCita();
+}
+
+
+function getAvisosList() {
+	mostrarAvisos();
+	/*$.getJSON(serviceocasionURL, function(data) {
+		$('#ocasionList li').remove();
+		eventos = data;
+		$.each(eventos, function(index, ocasion) {
+			$('#ocasionList').append('<li><a href="ocasiondetails.html?id=' + ocasion.id_casco + '">' +
+					'<img src="' + ocasion.ruta_imagen + '"/>' +
+					'<h4>' + ocasion.modelo + '</h4>' +
+					'<p>' + ocasion.kilometros + ' kil&oacute;metros - ' + ocasion.precio + '</p></li>');
+		});
+		$('#ocasionList').listview('refresh');
+	});*/
+}
+
+
+function getSesionForm() {
+	alert(1);
+	alert(sesion);
+	if((sesion!='')&&(sesion!='0'))
+	{
+		alert(2);
+		cerrar_sesion();
+		sesion = '';
+		alert('Sesión cerrada correctamente');
+		document.getElementById('mencerrar').id='meniniciar';
+		document.getElementById('meniniciar').innerHTML='Iniciar sesión';
+	}
+	mostrarSesion();
+}
+
+function mostrarDestacados()
+{
+  $('#apptitle').text('Destacados');
+  $('#contenedorDestacados').show();
+  $('#contenedorCita').hide();
+  $('#contenedorAvisos').hide();
+  $('#contenedorSesion').hide();
+}
+
+
+function mostrarCita()
+{
+  $('#apptitle').text('Solicitar Cita');
+  $('#contenedorDestacados').hide();
+  $('#contenedorCita').show();
+  $('#contenedorAvisos').hide();
+  $('#contenedorSesion').hide();
+}
+
+function mostrarAvisos()
+{
+  $('#apptitle').text('Notificaciones/Avisos');
+  $('#contenedorDestacados').hide();
+  $('#contenedorCita').hide();
+  $('#contenedorAvisos').show();
+  $('#contenedorSesion').hide();
+}
+
+
+function mostrarSesion()
+{
+  $('#apptitle').text('Iniciar Sesión');
+  $('#contenedorDestacados').hide();
+  $('#contenedorCita').hide();
+  $('#contenedorAvisos').hide();
+  $('#contenedorSesion').show();
+}
+
+function cerrar_sesion(){	 
+	 var xmlhttp =new XMLHttpRequest();
+	 xmlhttp.open("GET", "http://www.fulp.es/FULP/mensajesapp/registro_app.php?regId="+regId+"&cerrar=S",false);
+	 xmlhttp.send(null);	
+}
---
+++ www/js/index.js
@@ -0,0 +1,143 @@
+var app = { 
+	
+	
+    // Application Constructor 
+    initialize: function() { 
+
+		this.bindEvents(); 
+		
+    }, 
+    // Bind Event Listeners 
+    // 
+    // Bind any events that are required on startup. Common events are: 
+    // 'load', 'deviceready', 'offline', and 'online'. 
+    bindEvents: function() { 
+	
+        document.addEventListener('deviceready', this.onDeviceReady, false); 
+    }, 
+    // deviceready Event Handler 
+    // 
+    // The scope of 'this' is the event. In order to call the 'receivedEvent' 
+    // function, we must explicity call 'app.receivedEvent(...);' 
+    onDeviceReady: function() { 
+	
+        app.receivedEvent('deviceready'); 
+    }, 
+    // Update DOM on a Received Event 
+    receivedEvent: function(id) { 
+	
+        var parentElement = document.getElementById(id); 
+       /* var listeningElement = parentElement.querySelector('.listening'); 
+        var receivedElement = parentElement.querySelector('.received'); 
+        listeningElement.setAttribute('style', 'display:none;'); 
+        receivedElement.setAttribute('style', 'display:block;'); */
+
+        console.log('Received Event: ' + id);
+		//alert('Received Event: ' + id);
+		
+var pushNotification = window.plugins.pushNotification; 
+       // if (device.platform == 'android' || device.platform == 'Android') { 
+           // alert("Register called"); 
+            //tu Project ID aca!! 
+     pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"112340636347","ecb":"app.onNotificationGCM"}); 
+	 
+      /* } 
+        else { 
+            alert("Register called"); 
+            pushNotification.register(this.successHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"}); 
+        } */
+    }, 
+    // result contains any message sent from the plugin call 
+    successHandler: function(result) { 
+        //alert('Callback Success! Result = '+result) 
+    }, 
+    errorHandler:function(error) { 
+        alert(error); 
+    }, 
+    onNotificationGCM: function(e) { 
+		switch( e.event ) 
+        { 
+            case 'registered': 
+                if ( e.regid.length > 0 ) 
+                { 
+                    console.log("Regid " + e.regid); 
+                    //alert('registration id = '+e.regid); 
+                    //Cuando se registre le pasamos el regid al input 
+					var regId = e.regid;
+					
+                    document.getElementById('regId').value = regId; 
+					var sesion = '';		
+					
+					registrar_dispositivo();		
+					
+					setTimeout(comprobar_sesion(regId),10000);
+												
+                } 
+            break; 
+
+            case 'message': 
+              // NOTIFICACION!!! 
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt+' url = '+e.payload.url); 
+			  
+			  comprobar_sesion(regId);	
+			  
+			  if(e.payload.url){window.location.href = e.payload.url;}
+			  
+            break; 
+
+            case 'error': 
+              alert('GCM error = '+e.msg); 
+            break; 
+
+            default: 
+              alert('An unknown GCM event has occurred'); 
+              break; 
+        } 
+    }, 
+    onNotificationAPN: function(event) { 
+        var pushNotification = window.plugins.pushNotification; 
+        alert("Running in JS - onNotificationAPN - Received a notification! " + event.alert); 
+         
+        if (event.alert) { 
+            navigator.notification.alert(event.alert); 
+        } 
+        if (event.badge) { 
+            pushNotification.setApplicationIconBadgeNumber(this.successHandler, this.errorHandler, event.badge); 
+        } 
+        if (event.sound) { 
+            var snd = new Media(event.sound); 
+            snd.play(); 
+        } 
+    } 
+};
+
+function registrar_dispositivo(){	 
+	 var xmlhttp =new XMLHttpRequest();
+	 
+	 xmlhttp.open("GET", "http://www.fulp.es/FULP/mensajesapp/registro_app.php?regId="+document.getElementById("regId").value+"&cod_personal="+document.getElementById("cod_personal").value+"&new=S",false);
+
+		 xmlhttp.send(null);	
+}
+
+function comprobar_sesion(a)
+{
+	var dataString = "regId="+a+"&comprobarses=S";
+		$.ajax({ 
+            type: "POST",
+            url: "http://www.fulp.es/FULP/mensajesapp/registro_app.php",
+            data: dataString,
+            success: function(data) {
+				
+				if(data=='0')
+				{
+					document.getElementById("cod_personal").value=data;
+					setTimeout(document.getElementById('resgistro').submit(),10000);
+				}
+				else
+				{
+					document.getElementById("cod_personal").value=data;
+					setTimeout(document.getElementById('resgistro').submit(),10000);			
+				}
+            }
+        });
+}
