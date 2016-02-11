876c250fd6a934c6af77f41e1e9dc01bfb5a35ca
fulp2016 <webmaster@fulp.es>
2016-02-11 13:01:01 +0000

    v1

--- www/js/appsollist.js
+++ www/js/appsollist.js
@@ -12,7 +12,7 @@ $('#appsolListPage').bind('pageinit', function(event) {
 					$.each(data.items, function(index, video) {
 						 var id = video.id;
 						 var title = video.snippet.title;
-						 var imagen = video.thumbnails.default;
+						 var imagen = video.thumbnails.default.url;
 						 alert(imagen);
 						 $('#destacadosList').append('<a href="https://www.youtube.com/watch?v=' + id + '"><li>' +
 						'<h4><img src="'+ imagen +'"> ' + title + '</h4>' +
