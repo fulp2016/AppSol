5e2575fa171c29e3aea51ba2d421084bb4abef3c
fulp2016 <webmaster@fulp.es>
2016-02-15 14:40:07 +0000

    v1

--- www/js/appsollist.js
+++ www/js/appsollist.js
@@ -157,3 +157,25 @@ function change(a)
 	document.getElementById("pagina").value=a;
 	document.getElementById('irpagina').submit();
 }
+
+function getUrlVars()
+    {
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for(var i = 0; i < hashes.length; i++)
+        {
+            hash = hashes[i].split('=');
+
+            if($.inArray(hash[0], vars)>-1)
+            {
+                vars[hash[0]]+=","+hash[1];
+            }
+            else
+            {
+                vars.push(hash[0]);
+                vars[hash[0]] = hash[1];
+            }
+        }
+
+        return vars;
+    }
