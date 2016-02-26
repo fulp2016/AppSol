a65ee16486d9137bdee15c76fc4920c68fc4045c
fulp <webmaster@fulp.es>
2016-02-26 11:40:41 +0000

    v1

--- www/js/index.js
+++ www/js/index.js
@@ -76,9 +76,10 @@ var pushNotification = window.plugins.pushNotification;
             break; 
 
             case 'message': 
+			  if ( e.regid.length > 0 ) { var regId = e.regid; }
               // NOTIFICACION!!! 
               //alert('message = '+e.message+' msgcnt = '+e.msgcnt+' url = '+e.payload.url);   
-			  if(e.payload.url){ alert("1"+e.payload.url); document.getElementById('pagina').value=e.payload.url; /*$("#resgistro").attr("action",'pagina2.html');*/}
+			  if(e.payload.url){ document.getElementById('pagina').value=e.payload.url; /*$("#resgistro").attr("action",'pagina2.html');*/}
 			  setTimeout(comprobar_sesion(regId),10000);
             break; 
 
@@ -123,8 +124,6 @@ function comprobar_sesion(a)
             data: dataString,
             success: function(data) {
 				document.getElementById("cod_personal").value=data;
-				alert("2"+document.getElementById('pagina').value);
-				alert("3"+document.getElementById('cod_personal').value);
 				setTimeout(document.getElementById('resgistro').submit(),10000);
             }
         });
